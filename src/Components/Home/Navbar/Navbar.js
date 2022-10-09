525f5f336d4b29e3f86e0bfef08f97292da93c64
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-10-09 16:20:06 +0600

    navbar working progress

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -11,11 +12,12 @@ const Navbar = () => {
       }}
     >
       <nav className="w-[80%] mx-auto flex justify-between h-20 items-center  text-white">
-        <div className="text-2xl font-bold">logo</div>
+        <div className="text-2xl font-bold">Foodie</div>
+        <div className="flex">
         <ul className="flex mt-1">
           <li>
             <a
-              className="hover:text-red-500 duration-200 py-2 px-3 mr-3"
+              className="hover:text-orange-500 duration-200 py-2 px-3 mr-3"
               href="/home"
             >
               Home
@@ -23,7 +25,7 @@ const Navbar = () => {
           </li>
           <li>
             <a
-              className="hover:text-red-500 duration-200 py-2 px-3 mr-3"
+              className="hover:text-orange-500 duration-200 py-2 px-3 mr-3"
               href="/about"
             >
               About
@@ -31,7 +33,7 @@ const Navbar = () => {
           </li>
           <li>
             <a
-              className="hover:text-red-500 duration-200 py-2 px-3 mr-3"
+              className="hover:text-orange-500 duration-200 py-2 px-3 mr-3"
               href="/menu"
             >
               Menu
@@ -39,7 +41,7 @@ const Navbar = () => {
           </li>
           <li>
             <a
-              className="hover:text-red-500 duration-200 py-2 px-3 mr-3"
+              className="hover:text-orange-500 duration-200 py-2 px-3 mr-3"
               href="/services"
             >
               Services
@@ -47,28 +49,34 @@ const Navbar = () => {
           </li>
           <li>
             <a
-              className="hover:text-red-500 duration-200 py-2 px-3 mr-3"
+              className="hover:text-orange-500 duration-200 py-2 px-3 mr-3"
               href="/blogs"
             >
               Blogs
             </a>
           </li>
         </ul>
-        <ul className="flex">
+        <ul className="flex items-center justify-center">
           <li>
             <a className="" href="/cart">
-              <FaShoppingCart className="hover:text-red-500 text-2xl duration-200 mr-4" />
+              <FaShoppingCart className="hover:text-orange-500 text-2xl duration-200 mr-4" />
             </a>
           </li>
           <li>
             <a
-              className="py-2 px-3 mr-3 bg-red-500 hover:bg-red-500/80 rounded-[30px] duration-200"
+              className="py-2 px-3 mr-3 bg-orange-500 hover:bg-orange-500/80 rounded-[30px] duration-200"
               href="/booking"
             >
               Book a Table
             </a>
           </li>
+          <li>
+            <Link to="/" >
+              <img className="rounded-[50%] border-orange-500 border-4 hover:border-orange-500/80" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTJJjceD7Xn1-ZIEoxBUHl1f0lFXIi6daOmQ&usqp=CAU" alt="" width={40} height={40} />
+            </Link>
+          </li>
         </ul>
+        </div>
       </nav>
     </header>
   );
