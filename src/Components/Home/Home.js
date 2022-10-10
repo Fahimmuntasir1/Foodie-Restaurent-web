66e34a21a63e05272a36ef17be0d12cff9bc2927
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-10-10 23:11:20 +0600

    navbar banner video background added successfully

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Category from "./Category/Category";
-import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      {/* <Category></Category>
-      <Footer></Footer> */}
+      {/* <Main/> */}
+      {/* <Category/>
+      <Footer/> */}
     </div>
   );
 };
