88a9eabe45d871651a1cfeccb822d5a06356ee08
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-10-13 00:23:20 +0600

    navbar text -button adede

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Category from "./Category/Category";
 import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Category/>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 };
--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = () => {
             <li>
               <Link
                 to="/booking"
-                className="py-2 px-3 mr-3 text-center hidden lg:block bg-orange-500 hover:bg-orange-500/80 rounded-[30px] duration-200"
+                className="py-2 px-3 mr-3 text-center hidden lg:block bg-orange-500 hover:bg-orange-500/75 rounded-[30px] duration-200"
               >
                 Book a Table
               </Link>
@@ -122,6 +122,13 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
+      <div className="absolute w-[80%] ml-auto mr-auto left-0 right-0 text-center top-[40%]">
+      <h2 className="text-white text-6xl font-bold uppercase myFont">Family Owned. Locally Made. Community Focused.</h2> 
+      <div className="flex  justify-center flex-wrap">
+      <button className="text-white p-3 px-5 text-xl mr-3 bg-orange-500 mt-5 rounded hover:bg-orange-500/75">Book a Table</button>
+      <button className="text-white p-3 px-5 text-xl bg-orange-500 mt-5 rounded hover:bg-orange-500/75">Order from Home</button>
+      </div>
+      </div>
     </header>
   );
 };
