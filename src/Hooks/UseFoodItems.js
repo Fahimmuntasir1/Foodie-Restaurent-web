9082d8ee619de5aba0bafc82ae00322b08af8a7c
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-11-20 22:12:29 +0600

    backend connected and new data loaded

--- src/Hooks/UseFoodItems.js
+++ src/Hooks/UseFoodItems.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const UseFoodItems = () => {
-    const [foods, setFoods] = useState([])
-    useEffect(() => {
-        fetch('fakedata.json')
-        .then(res => res.json())
-        .then(data => setFoods(data))
-    }, [])
-    return [foods, setFoods]
+  const [foods, setFoods] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/foods")
+      .then((res) => res.json())
+      .then((data) => setFoods(data));
+  }, []);
+  return [foods, setFoods];
 };
 
 export default UseFoodItems;
