db6bb63fdcec2e54e03570af6b030cc96f58b34f
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-12-02 21:13:04 +0600

    initial data loaded

--- src/Components/Home/Category/AllFoods.js
+++ src/Components/Home/Category/AllFoods.js
@@ -2,23 +2,22 @@ import React from "react";
 import UseFoodItems from "../../../Hooks/UseFoodItems";
 import Food from "../Food/Food";
 
-const FoodItems = () => {
+const allFoods = () => {
   const [foods] = UseFoodItems([]);
   return (
-    <div className="w-[80%] mx-auto">
-      <div className="grid lg:grid-cols-3 gap-7">
+    <>
+      <div className="grid lg:grid-cols-3 gap-7 mx-auto ">
         {foods.slice(0, 9).map((food) => (
           <Food food={food} id={food.id}></Food>
         ))}
       </div>
-
       <div className="text-center my-9">
         <button className="text-white p-3 px-5 text-xl mr-3 bg-orange-500 mt-5 rounded hover:bg-orange-600">
           Load More
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
-export default FoodItems;
+export default allFoods;
