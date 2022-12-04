3ef68d60f598e9464999b8053d59a7e314f2ef38
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-12-04 21:36:30 +0600

    category nav buttons active route added

--- src/Components/Home/Category/Category.js
+++ src/Components/Home/Category/Category.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import burger from "../../../assests/burger.png";
 import desert from "../../../assests/desert.png";
 import frute from "../../../assests/frute.png";
@@ -12,41 +12,66 @@ const Category = () => {
         Top Categories
       </h2>
       <div className="flex w-[100%] text-center category justify-between mt-6 mb-20">
-        <Link
+        <NavLink
           to="/fastFood"
+          style={({ isActive }) =>
+            isActive
+              ? { backgroundColor: "#E6751F" }
+              : { backgroundColor: "#ff74115d" }
+          }
           className="flex bg-[#ff74115d] hover:bg-orange-400 duration-200 items-center justify-center p-[20px] w-[200px] rounded-[7px] font-semibold text-[22px]"
         >
           <h4>Fast Food</h4>
           <img className="ml-[8px] h-[40px] w-[40px]" src={burger} alt="" />
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/desserts"
+          style={({ isActive }) =>
+            isActive
+              ? { backgroundColor: "#E6751F" }
+              : { backgroundColor: "#ff74115d" }
+          }
           className="flex bg-[#ff74115d] hover:bg-orange-400 duration-200 items-center justify-center p-[20px] w-[200px] rounded-[7px] font-semibold text-[22px]"
         >
           <h4>Desserts</h4>
           <img className="ml-[8px] h-[40px] w-[40px]" src={desert} alt="" />
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/drinks"
+          style={({ isActive }) =>
+            isActive
+              ? { backgroundColor: "#E6751F" }
+              : { backgroundColor: "#ff74115d" }
+          }
           className="flex bg-[#ff74115d] hover:bg-orange-400 duration-200 items-center justify-center p-[20px] w-[200px] rounded-[7px] font-semibold text-[22px]"
         >
           <h4>Drinks</h4>
           <img className="ml-[8px] h-[40px] w-[40px]" src={desert} alt="" />
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/fruits"
+          style={({ isActive }) =>
+            isActive
+              ? { backgroundColor: "#E6751F" }
+              : { backgroundColor: "#ff74115d" }
+          }
           className="flex bg-[#ff74115d] hover:bg-orange-400 duration-200 items-center justify-center p-[20px] w-[200px] rounded-[7px] font-semibold text-[22px]"
         >
           <h4>Fruits</h4>
           <img className="ml-[8px] h-[40px] w-[40px]" src={frute} alt="" />
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/vegetables"
+          style={({ isActive }) =>
+            isActive
+              ? { backgroundColor: "#E6751F" }
+              : { backgroundColor: "#ff74115d" }
+          }
           className="flex bg-[#ff74115d] hover:bg-orange-400 duration-200 items-center justify-center p-[20px] w-[200px] rounded-[7px] font-semibold text-[22px]"
         >
           <h4>Vegetable</h4>
           <img className="ml-[8px] h-[40px] w-[40px]" src={vegetable} alt="" />
-        </Link>
+        </NavLink>
       </div>
       <Outlet />
     </section>
