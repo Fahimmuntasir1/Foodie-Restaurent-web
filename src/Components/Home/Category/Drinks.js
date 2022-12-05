fa33e805cb24cad1feeb28b85dde1abb13ed12b1
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-12-05 23:12:48 +0600

    unique key provided

--- src/Components/Home/Category/Drinks.js
+++ src/Components/Home/Category/Drinks.js
@@ -10,7 +10,7 @@ const Drinks = () => {
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-7 mx-auto ">
         {match.slice(0, 9).map((food) => (
-          <Food food={food} id={food._id}></Food>
+          <Food food={food} key={food._id}></Food>
         ))}
       </div>
       <div className="text-center my-9">
--- src/Components/Home/Category/Fruits.js
+++ src/Components/Home/Category/Fruits.js
@@ -9,7 +9,7 @@ const Fruits = () => {
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-7 mx-auto ">
         {match.slice(0, 9).map((food) => (
-          <Food food={food} id={food.id}></Food>
+          <Food food={food} key={food._id}></Food>
         ))}
       </div>
       <div className="text-center my-9">
